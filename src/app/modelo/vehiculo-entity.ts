8d359804b80bfbfd9ac0e00b6088135ab66159eb
Nemo <torrestjfelix@gmail.com>
2023-11-22 13:27:10 -0500

    aniadiendo el !

--- src/app/modelo/vehiculo-entity.ts
+++ src/app/modelo/vehiculo-entity.ts
@@ -1,9 +1,9 @@
 export class VehiculoEntity {
-    codvehiculo: string;
-    Modelo: string;
-    marca: string;
-    color: string;
-    traccion: string;
-    transmision: string;
-    asiento: number;
+    codvehiculo!: string;
+    Modelo!: string;
+    marca!: string;
+    color!: string;
+    traccion!: string;
+    transmision!: string;
+    asiento!: number;
 }
